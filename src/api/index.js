88eb060e0c32bf06ac736cbee93bc4d9bc4373e9
xunzou <xunzou@gmail.com>
2018-03-08 22:05:46 +0800

    add api

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: xunzou
+ * @Date: 2018-03-08 22:00:05
+ * @Desc: API
+ * @Last Modified by: xunzou
+ * @Last Modified time: 2018-03-08 22:01:39
+ */
+import login from './login'
+
+const api = {
+  login
+}
+
+export default api
--- src/api/login.js
+++ src/api/login.js
@@ -3,12 +3,49 @@
  * @Date: 2018-03-06 22:21:56
  * @Desc: login API
  * @Last Modified by: xunzou
- * @Last Modified time: 2018-03-06 22:39:06
+ * @Last Modified time: 2018-03-08 21:56:12
  */
 import axios from '@/utils/axios'
+/**
+ * 用户认证所用到的 API
+ */
+class AuthService {
+  /**
+   * 检测当前用户是否已经登录
+   * @resolve {Object} userData / null
+   */
+  checkLogin () {
+    return axios({
+      url: '/auth/checkLogin'
+    })
+  }
 
-export const login = () => {
-  return axios({
-    method: 'post'
-  })
+  /**
+   * 登录
+   * @param  {String} userData.username
+   * @param  {String} userData.password
+   * @return {Object} userData
+   */
+  login (userData) {
+    return axios({
+      method: 'post',
+      url: '/auth/login',
+      body: userData
+    })
+  }
+
+  /**
+   * 注销登录
+   * @return {Promise}
+   */
+  logout () {
+    return axios({
+      method: 'delete',
+      url: '/auth/logout'
+    })
+  }
+
 }
+
+// 实例化后导出，全局单例
+export default new AuthService()
